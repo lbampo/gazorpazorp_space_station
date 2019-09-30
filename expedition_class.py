c001544c603f0ce86c84eaad4959bfaed54d20db
Lennox Bampoe-Addo <LBampoe-Addo@spartaglobal.com>
2019-09-30 14:30:39 +0100

    Added code to expedtion class

--- expedition_class.py
+++ expedition_class.py
@@ -1,4 +1,10 @@
 #Expedition details should have:
+class Expedition():
+
+    def __init__(self, destination, spaceship, passenger_list, origin = 'Gazorpazorp Space station'):
+        self.destination = destination
+        self.spaceship = spaceship
+        self.passenger_list = []
 
 # A origin ( always gazorpazorp space station)
 # A destination
